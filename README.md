1ff753a7badf9430fb57bc0240773a60e89ba5ee
koturn <jeak.koutan.apple@gmail.com>
2016-05-17 01:22:51 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+BuildScript
+===========
+
+My build scripts.
+
+
+## LICENSE
+
+This software is released under the MIT License, see [LICENSE](LICENSE).
---
+++ vim/build.sh
@@ -0,0 +1,123 @@
+#!/bin/sh -eu
+
+show_usage() {
+  echo '[Usage]'
+  echo "  $0 [options] [arguments]"
+  echo '[Option]'
+  echo '  -c, --clean'
+  echo '    Delete configure cache'
+  echo '  -p PLATFORM, --platform=PLATFORM'
+  echo '    Specify platform'
+  echo '      cygwin: for cygwin'
+  echo '      linux: for linux (default)'
+  echo '  -h, --help'
+  echo '    Show help and exit'
+}
+
+case ${OSTYPE} in
+  cygwin | msys)
+    platform=cygwin
+    ;;
+  *)
+    platform=linux
+    ;;
+esac
+
+unset GETOPT_COMPATIBLE
+OPT=`getopt -o cp:h -l clean,platform:,help -- "$@"`
+if [ $? -ne 0 ]; then
+  echo >&2 'Invalid argument'
+  show_usage >&2
+  exit 1
+fi
+
+eval set -- "$OPT"
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    -c | --clean)
+      rm src/auto/config.cache
+      make clean
+      exit 0
+      ;;
+    -p | --platform)
+      platform=$2
+      shift
+      ;;
+    -h | --help)
+      show_usage
+      exit 0
+      ;;
+    --)
+      shift
+      break
+      ;;
+  esac
+  shift
+done
+
+build_cygwin() {
+  ./configure \
+    --prefix=/usr/local/ \
+    --enable-fail-if-missing \
+    --enable-gui=no \
+    --enable-multibyte=yes \
+    --enable-perlinterp=yes \
+    --enable-pythoninterp=yes \
+    --enable-rubyinterp=yes \
+    --enable-luainterp=yes \
+    --enable-tclinterp=yes \
+    --enable-cscope=yes \
+    --enable-gpm \
+    --enable-cscope \
+    --enable-fontset \
+    --with-features=huge \
+    --without-x \
+    --with-lua-prefix=/usr/local \
+    --with-modified-by=koturn \
+    --with-compiledby=koturn \
+    CFLAGS='-Ofast -march=native -mtune=native -funroll-loops -DNDEBUG' \
+    LDFLAGS='-s -Ofast' && \
+    make && \
+    make install
+}
+
+
+build_linux() {
+  ./configure \
+    --prefix=/usr/local/ \
+    --enable-fail-if-missing \
+    --enable-gui=yes \
+    --enable-multibyte=yes \
+    --enable-perlinterp=yes \
+    --enable-pythoninterp=yes \
+    --enable-rubyinterp=yes \
+    --enable-tclinterp=yes \
+    --enable-luainterp=yes \
+    --enable-cscope=yes \
+    --enable-mzschemeinterp=yes \
+    --enable-gpm \
+    --enable-cscope \
+    --enable-fontset \
+    --with-features=huge \
+    --with-x \
+    --with-modified-by=koturn \
+    --with-compiledby=koturn \
+    CFLAGS='-Ofast -m64 -march=native -mtune=native -funroll-loops -DNDEBUG' \
+    LDFLAGS='-s -Ofast' && \
+    make -j2 && \
+    make install
+}
+
+case $platform in
+  cygwin)
+    build_cygwin
+    ;;
+  linux)
+    build_linux
+    ;;
+  *)
+    echo 'Invalid platform name' >&2
+    ;;
+esac
+shift
